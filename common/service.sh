b1e25ce06910463c2520504f37fdc3409f1120aa
欣普 <15877161799@163.com>
2024-04-28 23:40:22 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -5,48 +5,55 @@
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
 
-UPERF="/storage/emulated/0/Android/yc/uperf/cur_powermode.txt"
-UPERFSET="/storage/emulated/0/Android/yc/uperf/perapp_powermode.txt"
-LOG="/storage/emulated/0/Android/perapp/log.txt"
-
-p_log () {
-    echo "[$(date +'%H:%M:%S')] $1" >> "$LOG"
-}
-
+# Wait for boot
 while true; do
     if [ "$(getprop sys.boot_completed)" == "1" ]; then
         break
     fi
     sleep 5
 done
-
 sleep 30
 
+
+# Start services
+
+# Define some paths
 MODDIR=${0%/*}
+STORAGE="/storage/emulated/0"
+LOG="$STORAGE/Android/perapp/log.txt"
+CurPath="$STORAGE/Android/perapp/uperf_cur_path.txt"
+ModePath="$STORAGE/Android/perapp/uperf_mode_path.txt"
 
-mkdir /storage/emulated/0/Android/perapp/
+# Default Uperf Path Settings
 
-p_log "Starting modules..."
-
-
-chmod a+x "$MODDIR/*.sh"
+UPERF="/storage/emulated/0/Android/yc/uperf/cur_powermode.txt"
+UPERFSET="/storage/emulated/0/Android/yc/uperf/perapp_powermode.txt"
+# Change permissions
 chmod 777 -R "$MODDIR"
+chmod 777 -R "$MODDIR/common"
 
-killProcess () {
-    ps -ef | grep "$MODDIR/ScreenStatCheck.sh" | awk '{print $2}'| while read pid;
-        do
-            kill -9 $pid >/dev/null 2>&1
-        done
+# Check uperf settings
+if [ ! -d "$STORAGE/Android/perapp" ] || [ ! -f "$CurPath" ] || [ ! -f "$ModePath" ]; then
+    mkdir $STORAGE/Android/perapp/
+    
+    echo "$UPERF" > "$CurPath"
+    echo "$UPERFSET" > "$ModePath"
+else
+    UPERF=$(cat "$CurPath")
+    UPERFSET=$(cat "$ModePath")
+fi
 
-    ps -ef | grep "$MODDIR/TopAppCheck.sh" |awk '{print $2}'| while read pid;
-        do
-            kill -9 $pid >/dev/null 2>&1
-        done
-}
+# Kill all processes to avoid running module repeatedly
+sh ./common/killAllPerAppLsnProcesses.sh
+sleep 1
+# Clean Log
+echo "" > "$LOG" 
 
-killProcess
-
-echo "Starting PerAppListener..." > "$LOG"
+echo "Uperf Cur Path: $UPERF"     >> "$LOG"
+echo "Uperf Mod Path: $UPERFSET"  >> "$LOG"
+echo "Starting PerAppListener..."
 
 nohup "$MODDIR/common/ScreenStatCheck.sh" > /dev/null 2>&1 &
 nohup "$MODDIR/common/TopAppCheck.sh" > /dev/null 2>&1 &
+
+echo "Loaded." >> "$LOG"
