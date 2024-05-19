a330ddda791c8f7382dc1b8bb9570cc5fcb06139
欣普 <15877161799@163.com>
2024-05-19 15:34:40 +0800

    Update TopAppCheck.sh

--- common/TopAppCheck.sh
+++ common/TopAppCheck.sh
@@ -2,7 +2,7 @@
 MODDIR=${0%/*}
 
 STORAGE="/storage/emulated/0"
-
+INOTIFYWAIT_PATH="$MODDIR/../system/xbin/inotifywait"
 
 LOG="$STORAGE/Android/perapp/log.txt"
 CurPath="$STORAGE/Android/perapp/uperf_cur_path.txt"
@@ -11,16 +11,16 @@ ModePath="$STORAGE/Android/perapp/uperf_mode_path.txt"
 # Default settings
 UPERF=$(cat "$CurPath")
 UPERFSET=$(cat "$ModePath")
+
+
 p_log () {
     echo "[$(date +'%H:%M:%S')] $1" >> "$LOG"
 }
 
-
-
-
 getTopApps () {
     echo "$(dumpsys activity activities | grep "VisibleActivityProcess" | grep -oE '[a-zA-Z0-9.]+/[a-zA-Z0-9.]+' | awk -F '/' '{print $1}')" 
 }
+
 # getAppMode "appname"
 getAppMode () {
     mode=$(grep "$1" "$UPERFSET" | awk '{print $2}')
@@ -31,7 +31,7 @@ prevTopApps=""
 
 p_log "Starting TopAppCheck..."
 
-inotifywait -m /dev/cpuset/top-app | while read x1 x2 x3; do
+$INOTIFYWAIT_PATH -m /dev/cpuset/top-app | while read x1 x2 x3; do
     # get top apps
     topApps=$(getTopApps)
     topApp=$(echo "$topApps" | awk 'NR==1')
