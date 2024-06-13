5579a684e08b8af80b947900a82827888d42845f
欣普 <15877161799@163.com>
2024-06-13 21:49:13 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -12,9 +12,20 @@ while true; do
     fi
     sleep 5
 done
-sleep 30
+
+wait_until_login() {
+    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
+    local test_file="/sdcard/Android/.PERMISSION_TEST"
+    true >"$test_file"
+    while [ ! -f "$test_file" ]; do
+        true >"$test_file"
+        sleep 1
+    done
+    rm "$test_file"
+}
 
 
+wait_until_login
 # Start services
 
 # Define some paths
